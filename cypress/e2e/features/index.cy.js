baed6811b5cc3b97ffe8448fbb00651cb1a6a67d
araxia555 <raymund.herradura.nz@gmail.com>
2024-12-05 10:06:40 +1300

    workflow: fix and add test

--- cypress/e2e/features/index.cy.js
+++ cypress/e2e/features/index.cy.js
@@ -2,5 +2,5 @@ it('titles are correct', () => {
     const page = cy.visit('http://localhost:4321');
   
     page.get('title').should('have.text', 'Ray')
-    page.get('h1').should('have.text', "Hello, I'm Ray.");
+    page.get('h1').should('have.text', "\nHello, I'm Ray.\n");
   });
