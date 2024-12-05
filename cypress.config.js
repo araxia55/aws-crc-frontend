b2b2475a05683516ad29872dd03235b6224d6093
araxia555 <raymund.herradura.nz@gmail.com>
2024-12-05 15:40:47 +1300

    remove files

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    supportFile: false,
-    // setup baseUrl for convenience
-    baseUrl: 'http://localhost:4321/', 
-  }
-})
--- cypress/e2e/features/index.cy.js
+++
@@ -1,6 +0,0 @@
-it('titles are correct', () => {
-    const page = cy.visit("/");
-  
-    page.get('title').should('have.text', 'Ray')
-    page.get('h1').should('have.text', "\nHello, I'm Ray.\n");
-  });
