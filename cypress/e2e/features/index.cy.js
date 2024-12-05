85898a2242bb25773f92155a58f1f4209351caa1
araxia555 <raymund.herradura.nz@gmail.com>
2024-12-05 17:16:53 +1300

    fix cypress config file

--- cypress/e2e/features/index.cy.js
+++ cypress/e2e/features/index.cy.js
@@ -1,5 +1,5 @@
 it('titles are correct', () => {
-    const page = cy.visit("/");
+    const page = cy.visit('http://localhost:4321', {retry: 3});
   
     page.get('title').should('have.text', 'Ray')
     page.get('h1').should('have.text', "\nHello, I'm Ray.\n");
