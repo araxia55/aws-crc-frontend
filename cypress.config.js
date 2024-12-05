b756ca37f566190a93cb3bfa81564dba3c40f3e8
araxia555 <raymund.herradura.nz@gmail.com>
2024-12-06 09:57:04 +1300

    fix deploy workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     supportFile: false,
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     // setup baseUrl for convenience
     // baseUrl: 'http://localhost:4321', 
   }
